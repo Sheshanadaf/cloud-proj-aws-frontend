987b32b5fd728da6d594b10216004532ae8a652c
Sheshanadaf <115085953+Sheshanadaf@users.noreply.github.com>
2025-02-03 19:06:33 +0530

    first commitasjddfllddf

--- scripts/build_react.sh
+++ scripts/build_react.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Build the React app (or Vite build)
-echo "Building the React app..."
+# Build the React app (or Vite build
+echo "Installing dependencies..."
+sudo yum install -y nodejs npm
 cd /home/ec2-user/app
+npm install
+echo "Building the React app..."
 npm run build  # Or use your Vite build command
